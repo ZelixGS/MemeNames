54eeddc7035ac4b1d646d8722ceb9ba9e6475262
Austin <54005277+ZelixGS@users.noreply.github.com>
2022-04-24 13:49:42 -0500

    Update 9.2
    Added Prefixes for Item Level
    Added more Items
    Fixed Profession Trainer Bug
    Fixed Items from Profession Tooltip Bug
    Fixed Edgecase Items behaving weirdly.
    Rewrote some Logic to be Cleaner, and more accepting to changes.
    Layed Groundwork for Suffixes.

--- MemeNames.lua
+++ MemeNames.lua
@@ -5,6 +5,20 @@ default_items = {
 	--Test Items
 	[39398]="Massive Cupsholder", 						-- Massive Skeletal Ribage
 	[39399]="Helm of Testing", 							-- Helm of the Vast Legions
+
+	--The Burning Crusader
+	[32757]="Medallion of the Tryhard Crusader", 		-- Blessed Medallion of Karabor
+	
+	--Warlords of Draenor
+	[109997]="Injector of Bombing Mk2",					-- Kihra's Adrenaline Injector
+	[110017]="Ambassador Pineapple", 					-- Enforcer's Stun Grenade
+
+	--Legion
+	[134542]="Forever Relevant :(",						-- Jeweled Signet of Melandrus
+	[137537]="Sleeper Tank Trinket",					-- Tirathon's Betrayal
+	[137539]="Icyhot",									-- Faulty Countermeasure
+	[137541]="Moonlit Middlefinger",					-- Moonlit Prism
+	
 	
 	--Shadowlands
 	[172051]="Steak and Mash", 							-- Steak a la Mode
@@ -27,15 +41,48 @@ default_items = {
 	[184020]="Phoenix Down", 							-- Tuft of Smoldering Plumage
 	[184024]={"Art Degree", "Toilet Paper"},			-- Macabre Sheet Music (First Random Item! 3/10/21)
 	[184026]="Ozma's Keelhauling Chain", 				-- Hateful Chain
-	--[184031]="Get you some Sip", 						-- Sanguine Vintage (Named by Kalcifur)
-	[184031]="Cum Chalice", 							-- Sanguine Vintage (Alternate Name)
+	[184031]={"Get you some Sip", "Cum Chalice"},		-- Sanguine Vintage (Named by Kalcifur)
 	[184840]="Book of Blasting Mk2", 					-- Hymnal of the Path
 	[184842]="Bell of Bombing", 						-- Instructor's Divine Bell
+	
+	-- 9.1 Items
+	--[186423]={"Worthy Eye of the Raidleader", "Unworthy Eye of the Raidleader"},		-- Titanic Ocular Gland
+	[186424]="Shard of Analhyde's Assgis",				-- Shard of Annhylde's Aegis
+	[186428]="Ponderous Orb of Torment",				-- Shadowed Orb of Torment
+	[186433]="Thorncoat", 								-- Reactive Defense Matrix
+	[187542]="Ozmthys, the Devouring Blade",			-- Jaithys, The Prison Blade
+	[186410]="Ozmthys, the Devouring Blade",			-- Jaithys, The Prison Blade (Again)
+	[186398]="Edge of Viability", 						-- Edge of Night
+	[186414]="Some bit of garbage",						-- Rae'shalare, Death's Whisper
+	[186438]="Orc yelling 'KEEP PULLING'",				-- Old Warrior's Soul		
+	[185783]="The REAL Legendary Bow",					-- Yasahm the Riftbreaker
+	[185818]="So'leah's Simping Technique", 			-- So'leah's Secret Technique
+	[185836]="Book of Blocking Mk12",					-- Codex of the First Technique
+	[190958]="So'leah's Simping Technique", 			-- So'leah's Secret Technique(There is 2 instances of this Item for Some Reason)
+
+	-- 9.2 Items
+	[188267]="Levithan's Lure Mk-2",					-- Bells of the Endless Feast
+	[188265]="Cache of No Axes?",						-- Cache of Acquired Treasures
+	[188266]="Ant-Squishing Riftshard",					-- Pulsating Riftshard
+	[188272]="Healer Trinket",							-- Resonant
+	[188270]="Elegy of the Cucked DPS",					-- Elegy of the Eternals
+	[188271]="Horn of Valor Mk2", 						-- The First Sigil
+	[188264]="Shakerpage", 								-- Earthbreaker's Impact
+	[188255]="NOT THE BEEEEEEEEES",						-- Heart of the Swarm
 }
+
+
 
 -- Used to Hold Table Information for Session and Random Names.
 items = {}
 ran_names = {}
+
+
+-- Script Variables
+giga_level = 278 -- Used to determine when something is max ilvl. Typically Mythic Raid ilvl.
+giga_name = "Giga"
+mega_level = 272
+mega_name = "Mega"
 
 
 --[[
@@ -43,10 +90,10 @@ ran_names = {}
 	First Determine if it's a Single Item, or Random.
 	If it's Random, we check our ran_names table to see if a value has been generated. If not, we Generate One.
 	Secondly, If we hold down the Alt Key, we add the Real Name in Parenthesis.
---]]
+	--]]
 local function GetNewName(id, fromChat)
-	local name
-	if type(items[id]) == "string" then 
+		local name
+		if type(items[id]) == "string" then 
 		name = items[id] 
 	end
 	if type(items[id]) == "table" then
@@ -62,6 +109,23 @@ local function GetNewName(id, fromChat)
 	return name
 end
 
+local function isGiga(ilvl)
+	if ilvl == nil then
+		return nil
+	end
+	if ilvl >= giga_level then
+		return giga_name.." "
+	elseif ilvl == mega_level then 
+		return mega_name.." "
+	end
+	return nil
+end
+
+-- Helper Function to get IDs from ItemLinks
+local function GetIDFromLink(msg)
+	local _, _, Color, Ltype, Id, Enchant, Gem1, Gem2, Gem3, Gem4, Suffix, Unique, LinkLvl, reforging, Name = string.find(msg, "|?c?f?f?(%x*)|?H?([^:]*):?(%d+):?(%d*):?(%d*):?(%d*):?(%d*):?(%d*):?(%-?%d*):?(%-?%d*):?(%d*):?(%d*)|?h?%[?([^%[%]]*)%]?|?h?|?r?")
+	return tonumber(Id)
+end
 
 --[[
 	Function to Modify tooltips, tooltips should contain information allowing us to get/set information, index is used to know which tooltip to modify.
@@ -71,10 +135,19 @@ end
 --]]
 local function ModTooltip(tooltip, index)
 	local link = select(2, tooltip:GetItem())
+	if link == nil then return end
+
 	local itemID = tonumber(string.match(link, "item:(%d*)"))
-	
-	if items[itemID] ~= nil then
-		local NewName = GetNewName(itemID, false)
+	if itemID ~= nil then
+		local ilvl = select(4, GetItemInfo(select(2, tooltip:GetItem())))
+		local NewName = C_Item.GetItemNameByID(itemID)
+		local Prefix = isGiga(ilvl)
+		if items[itemID] ~= nil then
+			NewName = GetNewName(itemID, false)
+		end
+		if Prefix ~= nil then
+			NewName = Prefix..NewName
+		end
 		local tooltipTitle = _G[tooltip:GetName() .. "TextLeft" .. index]
 		if tooltipTitle then
 			tooltipTitle:SetJustifyH("LEFT")
@@ -89,8 +162,8 @@ GameTooltip:HookScript("OnTooltipSetItem", function(self) ModTooltip(self, 1); e
 ItemRefTooltip:HookScript("OnTooltipSetItem", function(self) ModTooltip(self, 1); end)
 ItemRefShoppingTooltip1:HookScript("OnTooltipSetItem", function(self) ModTooltip(self, 2); end)
 ItemRefShoppingTooltip2:HookScript("OnTooltipSetItem", function(self) ModTooltip(self, 2); end)
-ShoppingTooltip1:HookScript("OnTooltipSetItem", function(self) ModTooltip(self, 2); end)
-ShoppingTooltip2:HookScript("OnTooltipSetItem", function(self) ModTooltip(self, 2); end)
+--ShoppingTooltip1:HookScript("OnTooltipSetItem", function(self) ModTooltip(self, 2); end)
+--ShoppingTooltip2:HookScript("OnTooltipSetItem", function(self) ModTooltip(self, 2); end)
 GameTooltip.ItemTooltip.Tooltip:HookScript("OnTooltipSetItem", function(self) ModTooltip(self, 1); end)
 
 --[[
@@ -106,9 +179,22 @@ local function EncounterJournalFrame(self)
 		local NewName = GetNewName(id, false)
 		local Quality = C_Item.GetItemQualityByID(self.link)
 		local Color = ITEM_QUALITY_COLORS[Quality].hex
-		self.name:SetText(Color..NewName)
+
+		if Quality ~= nil then
+			if id == 186414 then --Rae'shalare, Death's Whisper
+				Color = ITEM_QUALITY_COLORS[0].hex
+				self.lootFrame.IconBorder:SetVertexColor(0.6, 0.6, 0.6, 0.5)
+			end
+
+			if id == 185783 then --Yasahm the Riftbreaker
+				Color = ITEM_QUALITY_COLORS[5].hex
+				self.lootFrame.IconBorder:SetVertexColor(1, 0.5, 0, 0.5)
+			end
+		end
+		self.lootFrame.name:SetText(Color..NewName)
 	end
 end
+
 
 --[[
 	To Modify Chat Hyper Links, we first pass in a object, event, and message. 
@@ -123,19 +209,29 @@ local function ModChat(self, event, msg, ...)
 	while true do
 		i = string.find(msg, "|Hitem:", i+1)    -- find 'next' newline
 		if i == nil then break end
-		local _, _, Color, Ltype, Id, Enchant, Gem1, Gem2, Gem3, Gem4, Suffix, Unique, LinkLvl, reforging, Name = string.find(msg, "|?c?f?f?(%x*)|?H?([^:]*):?(%d+):?(%d*):?(%d*):?(%d*):?(%d*):?(%d*):?(%-?%d*):?(%-?%d*):?(%d*):?(%d*)|?h?%[?([^%[%]]*)%]?|?h?|?r?", i)
-		table.insert(links, tonumber(Id))
+		local chatitem = string.match(msg, "item[%-?%d:]+", i)
+		table.insert(links, chatitem)
 	end
 	
 	local msg2 = msg
 	for j, v in ipairs(links) do
-		if items[v] ~= nil then
-			local NewName = GetNewName(v, true)
-			msg2 = gsub(msg2, C_Item.GetItemNameByID(v), NewName)
+		local id = GetIDFromLink(v)
+		local NewName = C_Item.GetItemNameByID(id)
+		local Prefix = isGiga(GetDetailedItemLevelInfo(v))
+
+		if items[id] ~= nil then
+			NewName = GetNewName(id, true)
 		end
+
+		if Prefix ~= nil then
+			NewName = Prefix..NewName
+		end
+
+		msg2 = gsub(msg2, C_Item.GetItemNameByID(id), NewName)
 	end
 	return false, msg2, ...
 end
+
 
 --Chat Hooks
 ChatFrame_AddMessageEventFilter("CHAT_MSG_SAY",ModChat)
@@ -158,11 +254,6 @@ ChatFrame_AddMessageEventFilter("CHAT_MSG_INSTANCE_CHAT_LEADER",ModChat)
 ChatFrame_AddMessageEventFilter("CHAT_MSG_LOOT",ModChat)
 
 
--- Helper Function to get IDs from ItemLinks
-local function GetIDFromLink(msg)
-	local _, _, Color, Ltype, Id, Enchant, Gem1, Gem2, Gem3, Gem4, Suffix, Unique, LinkLvl, reforging, Name = string.find(msg, "|?c?f?f?(%x*)|?H?([^:]*):?(%d+):?(%d*):?(%d*):?(%d*):?(%d*):?(%d*):?(%-?%d*):?(%-?%d*):?(%d*):?(%d*)|?h?%[?([^%[%]]*)%]?|?h?|?r?")
-	return tonumber(Id)
-end
 
 
 
@@ -170,51 +261,51 @@ end
 	Used to Handle Chat Functionality, Renaming, Adding, Removing, Reset, and Re-Randomizing names.
 	Currently is a mess, might rewrite eventually? But it works.
 --]]
-SLASH_MEMENAME1 = '/mn'
-function SlashCmdList.MEMENAME(msg, editBox)
-	if string.find(string.upper(msg), "-HELP") then
-		print("MemeNames: To Add a Name, Link an Item and type it's new name afterwards.")
-		print("MemeNames: To Remove a Name, Link Item and Type '-clear' afterwards.")
-		return
-	end
-	if string.find(string.upper(msg), "-RANDOM") then
-		print("MemeNames: Re-Randomizing Items")
-		ran_names = {}
-		return
-	end
-    if string.find(msg, "|Hitem:") then
-		local ID = GetIDFromLink(msg)
-		local NewName = string.match(msg, "|r (.*)") --Unholy Regex/Pattern Matching
-		local OldName = C_Item.GetItemNameByID(ID)
-		if NewName == nil then
-			print("MemeNames: Error, No Name Found!")
-			return
-		elseif string.upper(NewName) == "-CLEAR" then
-			print("MemeNames: Removed "..OldName.."'s Meme Name!")
-			items[ID] = nil
-		elseif string.upper(NewName) == "-RESET" then
-			if default_items[ID] == nil then 
-				print("MemeNames: "..OldName.." has no default Name.")
-				return
-			end
-			items[ID] = default_items[ID]
-			print("MemeNames: Restoring "..OldName.." to "..items[ID].."")
-		elseif items[ID] ~= nil then
-			if type(items[ID]) == "string" then
-				temp = items[ID]
-				items[ID] = { temp, NewName}
-			else
-				table.insert(items[ID], NewName)
-			end
-			print("MemeNames: Added "..NewName.." as possibility for "..OldName.."")
-		else
-			print("MemeNames: Renamed "..OldName.." to "..NewName.."")
-			items[ID] = NewName
-		end
-	else
-		print("MemeName: Error, Missing Item Link!")
-	end
-end
+-- SLASH_MEMENAME1 = '/mn'
+-- function SlashCmdList.MEMENAME(msg, editBox)
+-- 	if string.find(string.upper(msg), "-HELP") then
+-- 		print("MemeNames: To Add a Name, Link an Item and type it's new name afterwards.")
+-- 		print("MemeNames: To Remove a Name, Link Item and Type '-clear' afterwards.")
+-- 		return
+-- 	end
+-- 	if string.find(string.upper(msg), "-RANDOM") then
+-- 		print("MemeNames: Re-Randomizing Items")
+-- 		ran_names = {}
+-- 		return
+-- 	end
+--     if string.find(msg, "|Hitem:") then
+-- 		local ID = GetIDFromLink(msg)
+-- 		local NewName = string.match(msg, "|r (.*)") --Unholy Regex/Pattern Matching
+-- 		local OldName = C_Item.GetItemNameByID(ID)
+-- 		if NewName == nil then
+-- 			print("MemeNames: Error, No Name Found!")
+-- 			return
+-- 		elseif string.upper(NewName) == "-CLEAR" then
+-- 			print("MemeNames: Removed "..OldName.."'s Meme Name!")
+-- 			items[ID] = nil
+-- 		elseif string.upper(NewName) == "-RESET" then
+-- 			if default_items[ID] == nil then 
+-- 				print("MemeNames: "..OldName.." has no default Name.")
+-- 				return
+-- 			end
+-- 			items[ID] = default_items[ID]
+-- 			print("MemeNames: Restoring "..OldName.." to "..items[ID].."")
+-- 		elseif items[ID] ~= nil then
+-- 			if type(items[ID]) == "string" then
+-- 				temp = items[ID]
+-- 				items[ID] = { temp, NewName}
+-- 			else
+-- 				table.insert(items[ID], NewName)
+-- 			end
+-- 			print("MemeNames: Added "..NewName.." as possibility for "..OldName.."")
+-- 		else
+-- 			print("MemeNames: Renamed "..OldName.." to "..NewName.."")
+-- 			items[ID] = NewName
+-- 		end
+-- 	else
+-- 		print("MemeName: Error, Missing Item Link!")
+-- 	end
+-- end
 
 
 --[[
@@ -228,9 +319,9 @@ local f = CreateFrame("Frame");
 function f:EventHandler(event, arg1, ...)
 	if event == "ADDON_LOADED" then
 		if arg1 == "MemeNames" then
-			if items[1] == nil then
+			--if items[1] == nil then
 				items = default_items
-			end
+			--end
 		end
 		if arg1 == "Blizzard_EncounterJournal" then
 			hooksecurefunc("EncounterJournal_SetLootButton", EncounterJournalFrame)
--- README.md
+++ README.md
@@ -2,12 +2,12 @@
 Written by _Zelix-Eldre'Thalas_.
 
 Renames Items in-game through Addon Functionality to allow Meme-y Names to be displayed instead of regular items names.
-Inspired by nicknaming the Hymnal of the Path to Book of Blasting, Phial to Granny Plague's Bathwater.
+Inspired by nicknaming the Hymnal of the Path to Book of Blasting, and Phial to Granny Plague's Bathwater.
 
 Features:
-* Renames Items.
+* Renames Items to (in)appropriate jargon, slang, or memes created by the <Utsukushii Saisei> guild.
 * Ability to add Random Name to Items that Changes Each Session.
-* Chat Commands to Rename, Add, Reset, Clear, and Re-Randomize names.
+* Adding Prefix to ALL items of appropriate, currently 272 (Mega) and 278+ (Giga)
 * Holding Alt will display the Item's Real Name as well.
 
 Where to find new Names:
@@ -17,20 +17,12 @@ Where to find new Names:
 	
 Planned Features (Maybe, and if I have time.):
 * Add Options UI
-* Effect More Frames
+* Effect More Frames (Weekly Vault / Guild Frame)
 * Allow Randomized Items to Reroll Periodically
-* In-Game Sharing and Syncing
+* In-Game Sharing and Syncing? (Maybe?)
 * Chaos Mode (Allow Random Names to not be session locked. Read FAQ for Details)
 * Spells
-	
-Commands | Effect
----------|---------
-/mn -help | Display's Commands.
-/mn -random | Rerolls Random Names.
-/mn Link Name | Sets and Items, or if an name is already set, add a random name.
-/mn Link -clear | Removes all names for the item.
-/mn Link -reset | Resets the Item back to the Default Name already programmed in the addon.
-		
+
 FAQ:
 * Does this break TOS?
   * No. This does not modify any files, and it strictly limited to Clients who have this AddOn installed and Enabled. Item Links do not change for other players without this addon. If Blizzard does not like the functionality they're free to break the methods used like previous brokenn addons.
@@ -38,3 +30,7 @@ FAQ:
 * Random isn't so Random?
   * Mousing Over Tooltips are constantly firing events to pick up if any Modifier Keys are Held Down. This will cause the Tooltip to jitter with random names. To Prevent this each Random Item is set to one of the Random Names upon seeing the item for the first time. Either Relog, ReloadUI, or type /mn -random, to reroll item names.
   * If I have time, I may add a "chaos" mode that will continously cycle through the names, or eventually I'll make it re-randomize every so often.
+
+* Do commands not work anymore?
+  * I've disabled commands for now, I _plan_ (read Eventually) to rewrite the process and put it in a UI, that also lists items.
+  * The main reason is so I didn't have to write an updater logic to handle new items being added and renamed.
